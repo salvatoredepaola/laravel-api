16b7817f9a94bcb84b908b4f10fbd2631f86f876
salvatoredepaola <salvatoredepaola8@gmail.com>
2023-07-10 22:07:57 +0200

    primo comit

---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Post;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i=0; $i < 10 ; $i++) { 
+            $post = new Post();
+            $post->title = $faker->sentence(3);
+            $post->content = $faker->text(500);
+            $post->image = $faker->imageUrl(800, 600, 'animals', true);
+            $post->save();
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,8 +19,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth'])
+		->prefix('admin') //definisce il prefisso "admin/" per le rotte di questo gruppo
+		->name('admin.') //definisce il pattern con cui generare i nomi delle rotte cioè "admin.qualcosa"
+		->group(function () {
+		
+			//Siamo nel gruppo quindi:
+			// - il percorso "/" diventa "admin/"
+			// - il nome della rotta ->name("dashboard") diventa ->name("admin.dashboard")
+			Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+
+            Route::resource('posts', PostController::class);
+
+	});
 
 require __DIR__.'/auth.php';
